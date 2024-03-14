4094ce7713b5138b55c7be8b75bbdd0762d595a1
Maryo <youcode098@gmail.com>
2024-03-14 22:17:27 +0000

    vercel configBack

--- Backend/index.js
+++ Backend/index.js
@@ -12,30 +12,29 @@ const skill = require("./Controllers/SkillController");
 const project = require("./Controllers/ProjectController");
 const contact = require("./Controllers/ContactController");
 
-
 const app = express();
 app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:3000", "https://portfolio-vervcel.vercel.app"],
+    credentials: true,
+  })
 );
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(process.env.DB)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`server rah mrani t7t lpor ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log("error hna", error);
+  .connect(process.env.DB)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server rah mrani t7t lpor ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log("error hna", error);
+  });
 
 app.use("/", auth);
 app.use("/", profile);
