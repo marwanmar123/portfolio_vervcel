fc66ea2ab35f8ac03f81fdd0b1c889d54c357b94
Maryo <youcode098@gmail.com>
2024-03-14 22:29:35 +0000

    url

--- frontend/src/Component/Auth/Register.js
+++ frontend/src/Component/Auth/Register.js
@@ -1,61 +1,92 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Register(props) {
-    const [inputValue, setInputValue] = useState({
-        username:"",
-        email:"",
-        password:""
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const { username, email, password } = inputValue;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
     });
+  };
 
-    const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const {username, email, password} = inputValue;
-
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setInputValue({
-            ...inputValue,
-            [name]:value
-        })
+    try {
+      await axios.post(
+        "https://portfolio-vervcel.vercel.app/register",
+        { ...inputValue },
+        { withCredentials: true }
+      );
+      navigate("/login");
+    } catch (err) {
+      console.log("errrrr");
     }
+    setInputValue({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try{
-            await axios.post("http://localhost:5000/register",{...inputValue}, {withCredentials:true});
-            navigate("/login")
-        }catch (err){
-            console.log("errrrr");
-        }
-        setInputValue({
-            username: "",
-            email:"",
-            password:""
-        })
-    }
-
-
-    return (
-        <form onSubmit={handleSubmit} className="w-50 m-auto pt-5">
-            <div className="mb-3">
-                <label className="form-label">Username</label>
-                <input type="text" name="username" value={username} onChange={handleChange} className="form-control" aria-describedby="emailHelp"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" name="email" value={email} onChange={handleChange} className="form-control"  aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" name="password" value={password} onChange={handleChange} className="form-control"/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} className="w-50 m-auto pt-5">
+      <div className="mb-3">
+        <label className="form-label">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+          className="form-control"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          className="form-control"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          className="form-control"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Register;
--- frontend/src/Context/UserContext.js
+++ frontend/src/Context/UserContext.js
@@ -1,40 +1,39 @@
-import React, {createContext, useContext, useEffect, useState} from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
-import {useCookies} from "react-cookie";
+import { useCookies } from "react-cookie";
 
 const UserData = createContext();
 
-export const UserContext = ({children}) => {
-    const [user, setUser] = useState({});
-    const [cookies] = useCookies(["token"])
+export const UserContext = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [cookies] = useCookies(["token"]);
 
-    useEffect(() => {
-        const getUser = async () => {
-            try{
-                if(cookies.token){
-                    const res = await axios.get("http://localhost:5000/me", {withCredentials:true})
-                    setUser(res.data.user);
-                }
-            }catch(er){
-                console.log("errror current user");
-            }
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        if (cookies.token) {
+          const res = await axios.get(
+            "https://portfolio-vervcel.vercel.app/me",
+            { withCredentials: true }
+          );
+          setUser(res.data.user);
         }
+      } catch (er) {
+        console.log("errror current user");
+      }
+    };
 
-        getUser();
-    }, [cookies]);
-    return (
-        <UserData.Provider value={{user}}>
-            {children}
-        </UserData.Provider>
-    );
-}
+    getUser();
+  }, [cookies]);
+  return <UserData.Provider value={{ user }}>{children}</UserData.Provider>;
+};
 
 export const useUser = () => {
-    const context = useContext(UserData);
-    if(!context){
-        console.log("context khaser");
-    }
-    return context;
-}
+  const context = useContext(UserData);
+  if (!context) {
+    console.log("context khaser");
+  }
+  return context;
+};
 
 export default UserContext;
