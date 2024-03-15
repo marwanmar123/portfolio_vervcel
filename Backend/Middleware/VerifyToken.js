b5dba82b03efcb7ee75519ffb852383cd629e3cb
Maryo <youcode098@gmail.com>
2024-03-15 02:59:32 +0000

    lg

--- Backend/Middleware/VerifyToken.js
+++ Backend/Middleware/VerifyToken.js
@@ -3,25 +3,26 @@ const User = require("../Models/Profile");
 require("dotenv").config();
 
 const VerifyToken = async (req, res, next) => {
-    //   console.log(req.cookies);
-    const token = req.cookies.token;
-    if (token) {
-        try {
-            const decoded = jwt.verify(token, process.env.JWT);
-            const user = await User.findById(decoded.userId);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-            if (!user) {
-                return res.status(400).json({ message: "user makanch" });
-            }
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token missing" });
+  }
 
-            req.user = user;
-            next();
-        } catch (error) {
-            return res.status(401).json({ message: error.message });
-        }
-    } else {
-        res.status(401).json({ message: "token makaynch" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 module.exports = VerifyToken;
